b95b3c113f6f23e439ff94e6467e6ade4d2fad16
Malak <malak.ismail@robustastudio.com>
2024-08-17 16:01:36 +0300

    feature: view of the daily orders report

---
+++ resources/views/emails/daily_orders_report.blade.php
@@ -0,0 +1,4 @@
+<h1>Daily Orders Report</h1>
+<p>Total Orders: {{ $orderCount }}</p>
+<p>Total Sales: {{ number_format($totalSales, 2) }}</p>
+<p>Please find the attached spreadsheet for detailed order information.</p>
